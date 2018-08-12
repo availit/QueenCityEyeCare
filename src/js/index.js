31bb5e65b47fe9da06e341026ef1eabab0a59392
availit <aaronvail14@gmail.com>
2018-08-11 23:38:19 -0500

    add paralax and build out navigation plus home page sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
+
+(function($){
+  $(function(){
+    $('.collapsible').collapsible();
+    $(".dropdown-trigger").dropdown({
+          inDuration: 300,
+          outDuration: 225,
+          constrainWidth: false, // Does not change width of dropdown to that of the activator
+          hover: true, // Activate on hover
+          gutter: 0, // Spacing from edge
+          belowOrigin: true, // Displays dropdown below the button
+          alignment: 'left' // Displays dropdown with edge aligned to the left of button
+        });
+    console.log('init parallax');
+    $('.parallax').parallax();
     $('.sidenav').sidenav();
     $('.materialboxed').materialbox();
     $('.modal').modal();
-    $('.parallax').parallax();
-  });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
